7e4e54abac07a2d5060a76e76d037591736f3666
Axel Naumann <Axel.Naumann@cern.ch>
2015-01-08 18:29:58 +0100

    Also store Parser paren numbers; likely fixes ROOT-6976:
    The parser (or rather the BalancedDelimiterTracker) was doing a Parser::cutOffParsing() because
    of an overflow of open ( - because we never reset the count.

--- clang/include/clang/Parse/Parser.h
+++ clang/include/clang/Parse/Parser.h
@@ -25,6 +25,8 @@
 #include <optional>
 #include <stack>
 
+namespace cling { class ParserStateRAII; }
+
 namespace clang {
   class PragmaHandler;
   class Scope;
@@ -65,6 +67,7 @@ class Parser : public CodeCompletionHandler {
   friend class ParenBraceBracketBalancer;
   friend class BalancedDelimiterTracker;
   friend class DestroyTemplateIdAnnotationsRAIIObj;
+  friend class ::cling::ParserStateRAII;
 
   Preprocessor &PP;
 
