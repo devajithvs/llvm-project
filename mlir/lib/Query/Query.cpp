6d7039cbe0ccac55085f442d1730b9e49421ca4a
Devajith <devajithvs@gmail.com>
2022-11-07 23:42:53 +0000

    [mlir][mlir-query] Introduce mlir-query tool with autocomplete support
    This commit adds the initial version of the mlir-query tool, which leverages the pre-existing matchers defined in mlir/include/mlir/IR/Matchers.h
    
    The tool provides the following set of basic queries:
    
    QUERY                     MATCHER
    hasOpAttrName(string)  -> m_Attr
    hasOpName(string)      -> m_Op
    isConstantOp()         -> m_Constant
    isNegInfFloat()        -> m_NegInfFloat
    isNegZeroFloat()       -> m_NegZeroFloat
    isNonZero()            -> m_NonZero
    isOne()                -> m_One
    isOneFloat()           -> m_OneFloat
    isPosInfFloat()        -> m_PosInfFloat
    isPosZeroFloat()       -> m_PosZeroFloat
    isZero()               -> m_Zero
    isZeroFloat()          -> m_AnyZeroFloat
    
    Differential Revision: https://reviews.llvm.org/D155127

---
+++ mlir/lib/Query/Query.cpp
@@ -0,0 +1,58 @@
+//===---- Query.cpp - mlir-query query --------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Query/Query.h"
+#include "mlir/Query/QuerySession.h"
+#include "llvm/Support/raw_ostream.h"
+
+namespace mlir::query {
+
+Query::~Query() {}
+
+bool InvalidQuery::run(llvm::raw_ostream &OS, QuerySession &QS) const {
+  OS << errStr << "\n";
+  return false;
+}
+
+bool NoOpQuery::run(llvm::raw_ostream &OS, QuerySession &QS) const {
+  return true;
+}
+
+bool HelpQuery::run(llvm::raw_ostream &OS, QuerySession &QS) const {
+  OS << "Available commands:\n\n"
+        "  match MATCHER, m MATCHER      "
+        "Match the mlir against the given matcher.\n\n";
+  return true;
+}
+
+std::vector<Operation *> getMatches(Operation *rootOp,
+                                    const matcher::DynMatcher &matcher) {
+  auto matchFinder = query::matcher::MatchFinder();
+  return matchFinder.getMatches(rootOp, matcher);
+}
+
+bool MatchQuery::run(llvm::raw_ostream &OS, QuerySession &QS) const {
+  Operation *rootOp = QS.rootOp;
+  auto matches = getMatches(rootOp, matcher);
+
+  MLIRContext *context = rootOp->getContext();
+  SourceMgrDiagnosticHandler sourceMgrHandler(*QS.sourceMgr, context);
+
+  unsigned matchCount = 0;
+  OS << "\n";
+  for (Operation *op : matches) {
+    OS << "Match #" << ++matchCount << ":\n\n";
+    // Placeholder "root" binding for the initial draft.
+    op->emitRemark("\"root\" binds here");
+  }
+  OS << matchCount << (matchCount == 1 ? " match.\n\n" : " matches.\n\n");
+
+  return true;
+}
+
+} // namespace mlir::query
