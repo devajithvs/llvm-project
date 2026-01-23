ed44cb3144215d0c46b837861bb277149b398170
Devajith Valaparambil Sreeramaswamy <devajith.valaparambil.sreeramaswamy@cern.ch>
2026-01-23 11:08:36 +0100

    Add test

---
+++ clang/test/Preprocessor/non-accessible-include.c
@@ -0,0 +1,10 @@
+// Needs chmod
+// UNSUPPORTED: system-windows
+//
+// RUN: chmod -R 755 %t
+// RUN: rm -rf %t && mkdir -p %t/noaccess %t/haveaccess
+// RUN: echo "int test();" > %t/haveaccess/test.h
+// RUN: chmod 000 %t/noaccess
+// RUN: %clang_cc1 -fsyntax-only -I %t/noaccess -I %t/haveaccess -verify %s
+
+#include "test.h" // expected-no-diagnostics
